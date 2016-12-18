da04770c34899e91a066983c9cac2a02df5e3f00
anhvurz90 <anhvurz90@gmail.com>
2016-12-18 16:22:04 +0700

    ReactJS - Component: Add Statefull example

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import App1 from './App1.jsx';
+import App2 from './App2.jsx';
 
 //ReactDOM.render(<App/>, document.getElementById("app"));
-ReactDOM.render(<App1/>, document.getElementById("app"));
+//ReactDOM.render(<App1/>, document.getElementById("app"));
+ReactDOM.render(<App2/>, document.getElementById("app"));
