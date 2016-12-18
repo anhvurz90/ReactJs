157f132e9118db9eafd01a26dc8080cb187a38bc
anhvurz90 <anhvurz90@gmail.com>
2016-12-18 16:35:29 +0700

    ReactJS - State:

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import App1 from './App1.jsx';
 import App2 from './App2.jsx';
+import App3 from './App3.jsx';
 
 //ReactDOM.render(<App/>, document.getElementById("app"));
 //ReactDOM.render(<App1/>, document.getElementById("app"));
-ReactDOM.render(<App2/>, document.getElementById("app"));
+//ReactDOM.render(<App2/>, document.getElementById("app"));
+ReactDOM.render(<App3/>, document.getElementById("app"));
