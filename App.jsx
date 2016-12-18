906daf9f4c62b1f54a82b68aecd6d37d72fafe74
anhvurz90 <anhvurz90@gmail.com>
2016-12-18 15:16:26 +0700

    ReactJs - JSX: Add Styling

--- App.jsx
+++ App.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 class App extends React.Component {
 	render() {
 		var i = 1;
+		
+		var myStyle = {
+			fontSize: 100,
+			color: '#FF0000'
+		}
+		
 		return (
 			<div>
-				<h1>Header</h1>
+				<h1 style={myStyle}>Header</h1>
 				<h2>Content</h2>
 				<p data-myattribute = "some value">This is the content!!!</p>
 				Hello World!!!
