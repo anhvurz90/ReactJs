3e78282b221c5834182dd5317ae4297d582bab12
anhvurz90 <anhvurz90@gmail.com>
2016-12-18 15:14:09 +0700

    ReactJs - JSX: add js expression

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 class App extends React.Component {
 	render() {
+		var i = 1;
 		return (
 			<div>
 				<h1>Header</h1>
 				<h2>Content</h2>
 				<p data-myattribute = "some value">This is the content!!!</p>
 				Hello World!!!
+				<p>{i == 1 ? 'One': 'not one'}</p>
 			</div>
 		);
 	}
