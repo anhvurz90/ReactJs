2fc8f30bd0fbb77ef2a6baa1b693350f7cdc5eaa
anhvurz90 <anhvurz90@gmail.com>
2016-12-24 21:01:17 +0700

    Add TutorialspointDoc: 07.PropsOverView: Props sample

--- 01.Tutorialspoint/main.js
+++ 01.Tutorialspoint/main.js
@@ -4,8 +4,12 @@ import App from './App.jsx';
 import App1 from './App1.jsx';
 import App2 from './App2.jsx';
 import App3 from './App3.jsx';
+import App4 from './App4.jsx';
 
 //ReactDOM.render(<App/>, document.getElementById("app"));
 //ReactDOM.render(<App1/>, document.getElementById("app"));
 //ReactDOM.render(<App2/>, document.getElementById("app"));
-ReactDOM.render(<App3/>, document.getElementById("app"));
+//ReactDOM.render(<App3/>, document.getElementById("app"));
+ReactDOM.render(<App4 headerProp="Header from props..."
+					contentProp="Content from props..."/>,
+				document.getElementById('app'));
