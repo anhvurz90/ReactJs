297266989637edcdc95d764bfe46171e8f7f8d3f
anhvurz90 <anhvurz90@gmail.com>
2016-12-24 21:21:05 +0700

    Update Tutorialspoint doc: PropsOverView

--- 01.Tutorialspoint/docs/tutorialspoint.java
+++ 01.Tutorialspoint/docs/tutorialspoint.java
@@ -311,10 +311,12 @@
 			import React from 'react';
 			class App extends React.Component {
 				render() {
-					<div>
-						<h1>{this.props.headerProp}</h1>
-						<h2>{this.props.contentProp}</h2>
-					</div>
+					return (
+						<div>
+							<h1>{this.props.headerProp}</h1>
+							<h2>{this.props.contentProp}</h2>
+						</div>
+					);
 				}
 			}
 			export default App;
