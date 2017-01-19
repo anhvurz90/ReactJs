6f2b98db504aaf9fc0560300ea81b69c6d11804e
anhvurz90 <anhvurz90@gmail.com>
2017-01-19 17:47:45 +0700

    Add Youtube2 doc:#01.Introduction

---
+++ 03.Youtube02/doc.java
@@ -0,0 +1,44 @@
+React Tutorial 
+https://www.youtube.com/watch?v=yZ0f1Apb5CU&list=PL4cUxeGkcC9i0_2FF-WhtRIfIJ1lXlTZR&index=1
+
+01.Introduction {
+	- What you need to know... {
+		+ HTML
+		+ JavaScript
+		+ Unafraid of using the command line (very simple)
+	}
+	- What is React? {
+		+ JavaScript library for creating user interfaces
+			* Full single page web applications
+			* Just certain parts of a website (like a search form)
+		+ Component-based
+		+ Very fast (thanks to the virtual DOM)
+		+ Created by Facebook
+	}
+	- JavaScript components: {
+		+ search component (1)
+		+ directory component (2)
+		+ signup component (3)
+		(1), (2), (3) -> HTML/ DOM
+	}
+	- What we will learn... {
+		+ Installing React and setting up a workflow with webpack
+		+ Create a simple to-do app, looking at:
+			* Components, props, state, events and more
+		+ Routing
+		+ Create-react-app
+	}
+	- https://nodejs.org/en -> download
+	- cmder.net -> for Windows users only
+	- atom: https://atom.io 
+	{
+		+ UI Theme: One Dark
+		+ Syntax Theme: Atom Monokai
+	}
+	- app overview: {
+		+ list todo
+		+ add todo
+		+ delete todo
+		+ routing
+	}
+}
